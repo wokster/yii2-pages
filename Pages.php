d1acd36cc83c891578b8fde353cf56a4ac2b2496
Максим Тимофеев <wokster@list.ru>
2017-05-05 18:15:44 +0300

    modul

---
+++ Pages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wokster\pages;
+
+class Pages extends \yii\base\Module
+{
+    public $controllerNamespace = 'wokster\pages\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
